3d19346e54a894444bb8dbda00ce843ef84dbb50
haavaal <haavaal@stud.ntnu.no>
2017-02-15 16:39:08 +0100

    added a test model named Contact.Added to admin and tested

--- CheckPoint/apps/snippets/models.py
+++ CheckPoint/apps/snippets/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Contact(models.Model):
 
+    first_name = models.CharField(
+        max_length=255,
+    )
+    last_name = models.CharField(
+        max_length=255,
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    )
+
+    email = models.EmailField()
+
+    def __str__(self):
+
+        return ' '.join([
+            self.first_name,
+            self.last_name,
+            self.email,
+        ])
--- CheckPoint/apps/snippets/tests.py
+++ CheckPoint/apps/snippets/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from models import Contact
 # Create your tests here.
+class ContactTests(TestCase):
+    """Contact model tests."""
+    def test_str(self):
+        contact = Contact(first_name='John', last_name='Smith',email='test@test.com')
+        self.assertEquals(
+            str(contact),
+            'John Smith test@test.com',
+        )
