0135fa9a5cf861feedf500ea4eaf1e4de51d5ce2
Edvin Johnsrud <edvin.johnsrud@gmail.com>
2017-02-18 14:52:29 +0100

    got a template to work on, working on the login functionality

--- CheckPoint/apps/loginPortal/views.py
+++ CheckPoint/apps/loginPortal/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse(render('index.html', request))
+    return render(request, 'myApp/login.html', {})
