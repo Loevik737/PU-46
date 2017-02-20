4ad97c29c79331a2ac693ea2847002d087e28a75
haavaal <haavaal@stud.ntnu.no>
2017-02-20 14:08:58 +0100

    an attempt to authenticae the user

--- CheckPoint/apps/snippets/views.py
+++ CheckPoint/apps/snippets/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from models import User
+from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
@@ -11,8 +11,11 @@ def show_users(request):
     # Obtain the context from the HTTP request.
     context = RequestContext(request)
     # Query the database for a list of ALL users currently stored.
-    users_list = User.objects.all()
+    if request.user.is_authenticated:
+        context_dict = {'user': user}
+    else:
+        context_dict = {'user': 'you are not authenticated'}
     # Place the list in our context_dict dictionary which will be passed to the template engine.
-    context_dict = {'users': users_list}
+
     # Render the response and send it back.
     return render_to_response('users/user.html', context_dict, context)
