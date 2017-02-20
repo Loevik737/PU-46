b6ab9e87ab47c98d0f857b95b9e5a5e8a2bee51f
haavaal <haavaal@stud.ntnu.no>
2017-02-20 13:11:12 +0100

    made the base template a separate file and changed the names of the templates

--- CheckPoint/apps/snippets/views.py
+++ CheckPoint/apps/snippets/views.py
@@ -15,4 +15,4 @@ def show_users(request):
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     context_dict = {'users': users_list}
     # Render the response and send it back.
-    return render_to_response('users/index.html', context_dict, context)
+    return render_to_response('users/user.html', context_dict, context)
