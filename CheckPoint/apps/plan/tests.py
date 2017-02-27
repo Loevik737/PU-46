06ad6351660fb342d91d42945e9c0a11276aa960
haavaal <haavaal@stud.ntnu.no>
2017-02-27 21:10:35 +0100

    made plan creation functionallity, fixed old tests and commented the code

--- CheckPoint/apps/plan/tests.py
+++ CheckPoint/apps/plan/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from CheckPoint.apps.subject.models import Subject
+from forms import CreatePlan
+import datetime
 # Create your tests here.
+class formTests(TestCase):
+
+    def test_ureatePlan(self):
+        valid_subject_pk = u1.pk
+        form = CreatePlan(title = 'Title', subject = str(valid_subject_pk), term='Vaar', year=datetime.datetime.now().year)
+        self.assertTrue(form.is_valid())
