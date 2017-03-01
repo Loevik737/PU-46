9a6a91b5d0e9c6b3101cbb95b7d4f0c4c23d63b9
oyvinabj <oyvinabj@stud.ntnu.no>
2017-03-01 15:36:44 +0100

    after login you get directed to home page

--- CheckPoint/apps/home/urls.py
+++ CheckPoint/apps/home/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from CheckPoint.apps.home.views import homeView
 #the url paths that sends you to the views you want
 urlpatterns = [
-    url(r'^$', homeView.as_view(), name='userIndex'),
+    url(r'^$', homeView.as_view(), name='home'),
 ]
