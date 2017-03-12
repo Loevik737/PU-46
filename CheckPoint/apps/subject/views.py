bfc2c2ba0e59ca251159f0fc3040af9d31ef8f60
oyvinabj <oyvinabj@stud.ntnu.no>
2017-03-12 21:33:47 +0100

    progress on subjectHome and views

--- CheckPoint/apps/subject/views.py
+++ CheckPoint/apps/subject/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from CheckPoint.apps.subject.models import Subject
 
 # Create your views here.
 
 def subjectView(request):
+
+    all_subjects = Subject.objects.all()
     return render(request, 'subjectHome.html',)
