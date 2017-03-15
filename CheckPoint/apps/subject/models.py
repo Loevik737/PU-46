3f80529a70311062b66297ddc0830a435fe26365
haavaal <haavaal@stud.ntnu.no>
2017-03-15 13:34:47 +0100

    added teacher to Subject model, and made it possible to edit in the admin panel

--- CheckPoint/apps/subject/models.py
+++ CheckPoint/apps/subject/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 class Subject(models.Model):
     code = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(User, related_name='subjectTeacher',null=True)
