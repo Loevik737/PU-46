2afffffbb122bf3a4682cf89bea8c9d8c565de45
haavaal <haavaal@stud.ntnu.no>
2017-03-20 22:23:29 +0100

    in the prosess of adding subjects to users

--- CheckPoint/apps/registration/admin.py
+++ CheckPoint/apps/registration/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from models import CostumUser
+from CheckPoint.apps.subject.models import Subject
 
-admin.site.register(CostumUser)
+class TeachSubjectInline(admin.TabularInline):
+    model = Subject
+    extra = 1 # how many rows to show
+
+class CostumUserAdmin(admin.ModelAdmin):
+    inlines = (TeachSubjectInline,)
+
+admin.site.register(CostumUser,CostumUserAdmin)
--- CheckPoint/apps/subject/models.py
+++ CheckPoint/apps/subject/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from CheckPoint.apps.registration.models import CostumUser
 
 class Subject(models.Model):
     code = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=100)
-    teacher = models.ForeignKey(User, related_name='subjectTeacher',null=True)
+    teacher = models.ForeignKey(CostumUser,limit_choices_to={'role': 'Teacher'}, related_name='subjectTeacher',null=True)
     def __unicode__(self):
        return 'Subject code: ' + self.code
