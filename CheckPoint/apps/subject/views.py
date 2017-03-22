1194336d1649ec6f6b003a1696d37de355dfa55c
oyvinabj <oyvinabj@stud.ntnu.no>
2017-03-22 14:41:56 +0100

    commented some code

--- CheckPoint/apps/subject/views.py
+++ CheckPoint/apps/subject/views.py
@@ -6,16 +6,17 @@ from CheckPoint.apps.plan.models import Plan
 # Create your views here.
 
 def subjectView(request):
+    #get costumuser
     user =request.user.costumuser
+    #get subjects the user attends
     all_subjects = user.attendingSubject.all
-    #subject_list = []
-    #for object in all_subjects:
-        #subject_list.append(object.attendingSubject)
+    #get all plan objects
     plan = Plan.objects.all()
+    #make dictionary where key is subjectname in plan and value is planID
     plansubject = {}
     for object in plan:
         plansubject[object.subject.name]=object.pk
+    #make user subjects, plan objects and plansubject dictionary usable from template
     args = {'subjects': all_subjects, 'plan': plan, 'plansubject':plansubject, }
-    #all_plans = Plan.objects.all()
-    #context = {'plans' : all_plans }
+    #render subjectHome.html
     return render(request, 'subjectHome.html', args)
