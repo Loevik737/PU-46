40b01e4acaaa189a4b0e104924c2466e195ef957
haavaal <haavaal@stud.ntnu.no>
2017-03-22 12:38:39 +0100

    admin can now ad subjects to users

--- CheckPoint/apps/registration/admin.py
+++ CheckPoint/apps/registration/admin.py
@@ -3,10 +3,11 @@ from models import CostumUser
 from CheckPoint.apps.subject.models import Subject
 
 class TeachSubjectInline(admin.TabularInline):
-    model = Subject
+    model = CostumUser.attendingSubject.through
     extra = 1 # how many rows to show
 
 class CostumUserAdmin(admin.ModelAdmin):
     inlines = (TeachSubjectInline,)
+    exclude = ('attendingSubject',)
 
 admin.site.register(CostumUser,CostumUserAdmin)
--- CheckPoint/apps/registration/models.py
+++ CheckPoint/apps/registration/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-
 class CostumUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=30)
